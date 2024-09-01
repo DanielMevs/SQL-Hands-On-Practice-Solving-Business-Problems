50f44679cbc16b31c45bc70864d7d3e74fe865e7
DanielMevs <dannymevs@hotmail.com>
2024-08-31 20:46:45 -0400

    fixed bug

--- CASE/binary_columns.sql
+++ CASE/binary_columns.sql
@@ -1,13 +1,12 @@
 SELECT
-    customerid,
-    COUNT(productId) AS num_products,
-    SUM(numberOfUsers) AS total_users
-    CASE WHEN COUNT(productId) = 1
-        OR SUM(numberOfUsers) >= 5000
-        THEN 1
-        ELSE 0
-        END AS upsell_opportunity
+    CUSTOMERID,
+    COUNT(PRODUCTID) AS num_products,
+    SUM(NUMBEROFUSERS) AS total_users
+    CASE WHEN COUNT(PRODUCTID) = 1
+        OR SUM(NUMBEROFUSERS) >= 5000
+        THEN 1 ELSE 0
+    END AS upsell_opportunity
 FROM
-    subscriptions
+    SUBSCRIPTIONS
 GROUP BY
-    customerId
+    CUSTOMERID;
